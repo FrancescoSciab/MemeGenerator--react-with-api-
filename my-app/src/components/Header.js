623fed51b015ab5f8a690df3aadae44fe8e5f51a
FrancescoSciab <francesco.sciabbarrasii@gmail.com>
2023-03-12 13:18:03 +0000

    creating

---
+++ my-app/src/components/Header.js
@@ -0,0 +1,10 @@
+
+export default function Header() {
+    return (
+        <header>
+            <h2>Meme Generator</h2>
+
+            <h4>React course - project</h4>
+        </header>
+    )
+}
---
+++ my-app/src/components/Meme.js
@@ -0,0 +1,29 @@
+
+
+export default function Meme(){
+    return (
+       <main className='content'>
+
+            <div className='form'>
+                <input
+                    type="text"
+                    placeholder="Top text"
+                />
+                <input
+                    type="text"
+                    placeholder="Bottom text"
+                />
+
+                <button className='form-button'>Generate meme</button>
+            </div>
+
+
+            
+                
+            
+            <div className='meme-section'>
+
+            </div>
+       </main>
+    )
+}
